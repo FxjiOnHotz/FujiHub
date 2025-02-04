c8f5f365cbc6d6ea635faf4031a0024b92b5fbba
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-03 21:16:53 -0500

    Update and rename temp.lua to FleeTheFacility.lua

---
+++ Supported/FleeTheFacility.lua
@@ -0,0 +1,34 @@
+local CustomTheme = {
+    SchemeColor = Color3.fromRGB(31, 51, 80),
+    Background = Color3.fromRGB(25, 25, 25),
+    Header = Color3.fromRGB(20, 20, 20),
+    TextColor = Color3.fromRGB(255, 255, 255),
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("FujiHub v1 | Flee The Facility 🔨", CustomTheme)
+
+local MainTab = Window:NewTab("Main")
+
+local MainSection = MainTab:NewSection("Game Info")
+
+-- Main tab
+
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+MainSection:NewButton("Game Name: " .. GameName, "The game's name", function()
+    toClipboard(game.Name)
+end)
+
+MainSection:NewButton("PlaceId: " .. game.PlaceId, "The game's place id", function()
+    toClipboard(game.PlaceId)
+end)
+
+MainSection:NewButton("JobId: " .. game.JobId, "The game's job id", function()
+    toClipboard(game.JobId)
+end)
+
+MainSectionUI:NewKeybind("Toggle UI", "Key to toggle the gui.", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
