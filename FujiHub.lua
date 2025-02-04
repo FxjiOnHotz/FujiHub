1fe901a47b2c6896df11247814dfa7a102d0b33b
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-03 21:15:43 -0500

    Update FujiHub.lua

--- FujiHub.lua
+++ FujiHub.lua
@@ -1,5 +1,5 @@
 local CustomTheme = {
-    SchemeColor = Color3.fromRGB(161, 3, 248),
+    SchemeColor = Color3.fromRGB(31, 51, 80),
     Background = Color3.fromRGB(25, 25, 25),
     Header = Color3.fromRGB(20, 20, 20),
     TextColor = Color3.fromRGB(255, 255, 255),
@@ -24,7 +24,7 @@ KeySection:NewTextBox("Enter Key", "Your key goes here.", function(txt)
 	EnteredKey = txt
     if EnteredKey == Key then
         Library:ToggleUI()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/FxjiOnHotz/FujiHub/refs/heads/main/Resources/GameSelector.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/FxjiOnHotz/FujiHub/refs/heads/main/Resources/GameSelector.lua"))()
     else
         print("The key you have entered for FujiHub is incorrect.")
     end
