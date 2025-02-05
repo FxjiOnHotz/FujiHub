643244f0f2ab3ec77a52fb3d460c229449895214
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-04 22:58:31 -0500

    Update AutomaticLoader.lua

--- Versions/AutomaticLoader.lua
+++ Versions/AutomaticLoader.lua
@@ -4,6 +4,8 @@ elseif game.PlaceId == 142823291 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/FxjiOnHotz/FujiHub/refs/heads/main/Supported/MurderMystery2.lua"))()
 elseif game.PlaceId == 893973440 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/FxjiOnHotz/FujiHub/refs/heads/main/Supported/FleeTheFacility.lua"))()
+elseif game.PlaceId == 338521019 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/FxjiOnHotz/FujiHub/refs/heads/main/Supported/IdentityFraud.lua"))()
 else
   return "FujiHub doesn't support that game!"
 end
