97f9474d939de00a63abdd0708274acb481d084c
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-10 20:31:58 -0500

    Update DandysWorld.lua

--- Supported/DandysWorld.lua
+++ Supported/DandysWorld.lua
@@ -1,5 +1,5 @@
 local CustomTheme = {
-    SchemeColor = Color3.fromRGB(208, 3, 255),
+    SchemeColor = Color3.fromRGB(5, 233, 36),
     Background = Color3.fromRGB(25, 25, 25),
     Header = Color3.fromRGB(20, 20, 20),
     TextColor = Color3.fromRGB(255, 255, 255),
