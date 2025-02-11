b910a625167daf53778003b3789c90e76a221fd3
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-10 20:31:29 -0500

    Update IdentityFraud.lua

--- Supported/IdentityFraud.lua
+++ Supported/IdentityFraud.lua
@@ -1,5 +1,5 @@
 local CustomTheme = {
-    SchemeColor = Color3.fromRGB(208, 3, 255),
+    SchemeColor = Color3.fromRGB(194, 187, 73),
     Background = Color3.fromRGB(25, 25, 25),
     Header = Color3.fromRGB(20, 20, 20),
     TextColor = Color3.fromRGB(255, 255, 255),
