3af8ee946c18d66a1b584a009c1f33762633daf2
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-19 23:18:31 -0500

    Update ShardSeekers.lua

--- Supported/ShardSeekers.lua
+++ Supported/ShardSeekers.lua
@@ -14,18 +14,110 @@ local Window = Library.CreateLib("FujiHub v1 | Shard Seekers ✨", CustomTheme)
 local MainTab = Window:NewTab("Main")
 local VisualsTab = Window:NewTab("Visuals")
 local UtiltiesTab = Window:NewTab("Utilties")
+local AutosTab = Window:NewTab("Autos")
+local GameTab = Window:NewTab("Game")
 
 -- Sections
 
 local MainSection = MainTab:NewSection("Game Info")
 local MainSectionUI = MainTab:NewSection("UI")
 local VisualsSection = VisualsTab:NewSection("Visuals")
-local UtilitiesSection = VisualsTab:NewSection("Utilties")
+local UtilitiesSection = UtiltiesTab:NewSection("Utilties")
+local AutosSection = AutosTab:NewSection("Autos")
+local GameSection = GameTab:NewSection("Game")
+
+-- Autos Tab
+
+local autoFenabled = false
+
+AutosSection:NewToggle("Auto Forage", "", function(state)
+    autoFenabled = state
+    if autoFenabled then
+        task.spawn(function()
+            while autoFenabled do
+                for i, object in pairs(game.Players.LocalPlayer.PlayerGui:GetDescendants()) do
+                    if object:IsA("TextLabel") or object:IsA("TextButton") then
+                        if object.Text == "Forage" then
+                            game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.E, false, game)
+                            wait(0.1)
+                            game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.E, false, game)
+                        end
+                    end
+                end
+                task.wait(0.1)
+            end
+        end)
+    end
+end)
+
+local autoHenabled = false
+
+AutosSection:NewToggle("Auto Hunt", "", function(state)
+    autoHenabled = state
+    if autoHenabled then
+        task.spawn(function()
+            while autoHenabled do
+                for i, object in pairs(game.Players.LocalPlayer.PlayerGui:GetDescendants()) do
+                    if object:IsA("TextLabel") or object:IsA("TextButton") then
+                        if object.Text == "Hunt" then
+                            game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.E, false, game)
+                            wait(0.1)
+                            game:GetService("VirtualInputManager"):SendKeyEvent(false, Enum.KeyCode.E, false, game)
+                        end
+                    end
+                end
+                task.wait(0.1)
+            end
+        end)
+    end
+end)
+
+-- Game Tab
+
+GameSection:NewButton("Rejoin", "Rejoins the game.", function()
+    repeat
+        wait()  
+    until game:IsLoaded() 
+    game:GetService("TeleportService"):Teleport(340416369) 
+    
+end)
 
 -- Utilities Tab
 
-VisualsSection:NewToggle("Auto Interact Prompt", "", function(state)
+local autoReachFalseToggle = true
+local autoRenabled = false
 
+UtilitiesSection:NewButton("Fullbright", "Makes everything bright", function()
+    -- FULLBRIGHT by ricey#7090
+        pcall(function()
+            local lighting = game:GetService("Lighting");
+            lighting.Ambient = Color3.fromRGB(255, 255, 255);
+            lighting.Brightness = 1;
+            lighting.FogEnd = 1e10;
+            for i, v in pairs(lighting:GetDescendants()) do
+                if v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("SunRaysEffect") then
+                    v.Enabled = false;
+                end;
+            end;
+            lighting.Changed:Connect(function()
+                lighting.Ambient = Color3.fromRGB(255, 255, 255);
+                lighting.Brightness = 1;
+                lighting.FogEnd = 1e10;
+            end);
+        end)
+    end)
+
+UtilitiesSection:NewToggle("Auto Interact Prompt", "", function(state)
+    autoRenabled = state
+    if autoRenabled then
+        task.spawn(function()
+            while autoRenabled do
+                autoReachFalseToggle = not autoReachFalseToggle
+                game:GetService("VirtualInputManager"):SendKeyEvent(autoReachFalseToggle, Enum.KeyCode.E, false, game)
+                task.wait(0.01)
+            end
+        end)
+    end
 end)
 
 -- Visuals Tab
