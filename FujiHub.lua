4cf9e5adc4162734584a8f1c01e6d1d66d7f9171
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-22 19:07:12 -0500

    Update FujiHub.lua

--- FujiHub.lua
+++ FujiHub.lua
@@ -59,19 +59,4 @@ Tabs.Info:AddParagraph({
     Content = "This Roblox script was made out of pure premium scripting and gives the user a premium-experience and make them overpowered."
 })
 
-Tabs.Info:AddParagraph({
-    Title = "The Goal",
-    Content = "The goal is to make players have fun playing Roblox because truth be told; it isn't as fun anymore. Roblox used to be fun and we made a lot of memories from it. But now when you grow up, your memories aren't the same. Your childhood is gone."
-})
-
-Tabs.Info:AddParagraph({
-    Title = "How To Get The Key",
-    Content = "To get the key, press on the 'Key' tab and press on get key. It copies a link to your clipboard which you paste in your browser. However don't worry about any linkvertises or lootlink's or anything like that. Over here, we want you to trust us and value our users."
-})
-
--- Credits Tab
-
-Tabs.Credits:AddParagraph({
-    Title = "Credits",
-    Content = "Discord:\n\nf4jii. - Owner/Scripter/Developer \n\n Other: \n Kavo UI Library - Princelion33 \n Flunet UI Library - dawid"
-})
+Window:SelectTab(1)
