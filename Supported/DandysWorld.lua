a3fa03d842637383669ab744db4b1afe312e0c8e
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-28 23:57:30 -0500

    Update DandysWorld.lua

--- Supported/DandysWorld.lua
+++ Supported/DandysWorld.lua
@@ -7,7 +7,7 @@ local CustomTheme = {
 }
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("FujiHub v1 | Dandy's World 🌈", CustomTheme)
+local Window = Library.CreateLib("FujiHub v1 | Dandy's World", CustomTheme)
 
 -- Tabs
 
