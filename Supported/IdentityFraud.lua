33e05f4bbaea2fb02c4bed8b8e8eb20c4a7875bd
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-28 23:57:57 -0500

    Update IdentityFraud.lua

--- Supported/IdentityFraud.lua
+++ Supported/IdentityFraud.lua
@@ -7,7 +7,7 @@ local CustomTheme = {
 }
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("FujiHub v1 | Identity Fraud 🖥️", CustomTheme)
+local Window = Library.CreateLib("FujiHub v1 | Identity Fraud", CustomTheme)
 
 local MainTab = Window:NewTab("Main")
 local PlayerTab = Window:NewTab("Player")
