946ad7287f520c8632be106ad79ffd13c227e2ae
FxjiOnHotz <167031962+FxjiOnHotz@users.noreply.github.com>
2025-02-28 23:57:22 -0500

    Update DOORS.lua

--- Supported/DOORS.lua
+++ Supported/DOORS.lua
@@ -7,7 +7,7 @@ local CustomTheme = {
 }
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("FujiHub v1 | DOORS 👁️", CustomTheme)
+local Window = Library.CreateLib("FujiHub v1 | DOORS", CustomTheme)
 
 -- Tabs
 
